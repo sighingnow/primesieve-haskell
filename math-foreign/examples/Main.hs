5a4c0dceee35f5849f42cde64151320ecb40680e
HE, Tao <sighingnow@gmail.com>
2017-10-14 16:49:14 +0800

    Make the example compilable based on the intuition plugin.
    Signed-off-by: HE, Tao <sighingnow@gmail.com>

--- math-foreign/examples/Main.hs
+++ math-foreign/examples/Main.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -fplugin Plugin.Intuition -fplugin-opt Plugin.Intuition:--nodebug #-}
+{-# OPTIONS_GHC -fplugin Plugin.Intuition.KnownNat #-}
+{-# OPTIONS_GHC -fprint-potential-instances #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedLists #-}
 
 module Main where
@@ -5,12 +9,18 @@ module Main where
 import Foundation
 import qualified Math.Linear.Matrix.Mutable as MM
 import Math.Complex
+import Math.Linear.Vector
 import Math.Linear.Matrix
 import Math.Linear.Linalg
 
+import Foundation
+import Foundation.Primitive
+
+import Plugin.Intuition.KnownNat
+
 app2 :: IO ()
 app2 = do
-    m <- MM.ones 3 4 :: IO (MM.IOMat Float)
+    m <- MM.ones Proxy Proxy :: IO (MM.IOMat Float 3 4)
     MM.shift 4 m
     MM.times 3 m
     -- putStrLn . show $ m
@@ -23,7 +33,7 @@ app2 = do
 
 app4 :: IO ()
 app4 = do
-    let a = identity 4 4 :: Mat Float
+    let a = identity Proxy Proxy :: Mat Float 4 4
     putStrLn . show $ a
     putStrLn . show $ a `add` a
     let d = dot a a
@@ -32,58 +42,59 @@ app4 = do
 
 app5 :: IO ()
 app5 = do
-    let x = ones 4 4 :: Mat (Complex Float)
-        y = ones 4 4 :: Mat (Complex Float)
+    let x = ones Proxy Proxy :: Mat (Complex Float) 4 4
+        y = ones Proxy Proxy :: Mat (Complex Float) 4 4
     putStrLn . show $ x `dot` y
     putStrLn . show $ det x
     putStrLn . show $ det (x `dot` y)
-    let z = fromList' 3 3 [1.0, 1.0, 1.0, 3.0, 2.0, 3.0, 7.0, 8.0, 9.0] :: Mat Float -- (Complex Float)
-    putStrLn . show $ (z `at` (1, 2))
+    let z = fromList' 3 3 [1.0, 1.0, 1.0, 3.0, 2.0, 3.0, 7.0, 8.0, 9.0] :: Mat Float 3 3 -- (Complex Float)
+    putStrLn . show $ (z `at` (Proxy :: Proxy 1, Proxy :: Proxy 2))
     putStrLn . show $ det z
     putStrLn . show $ rank z
     putStrLn . show $ inverse z
 
 app6 :: IO ()
 app6 = do
-    let x = fromList' 3 2 [ 1.54818221,  0.42103339, -0.18791299,  0.72404307, -0.99845554, -0.2456467 ] :: Mat Float
+    let x = fromList' 3 2 [ 1.54818221,  0.42103339, -0.18791299,  0.72404307, -0.99845554, -0.2456467 ] :: Mat Float 3 2
         y = transpose x
         (qx, rx) = qr x
-        (qy, ry) = qr y
-        (qx', rx') = qr' x
-        (qy', ry') = qr' y
+        -- (qy, ry) = qr y
+        -- (qx', rx') = qr' x
+        -- (qy', ry') = qr' y
+    -- return ()
     putStrLn . show $ qx
-    putStrLn . show $ rx
-    putStrLn . show $ qy
-    putStrLn . show $ ry
-    putStrLn . show $ qx'
-    putStrLn . show $ rx'
-    putStrLn . show $ qy'
-    putStrLn . show $ ry'
+    -- putStrLn . show $ rx
+    -- putStrLn . show $ qy
+    -- putStrLn . show $ ry
+    -- putStrLn . show $ qx'
+    -- putStrLn . show $ rx'
+    -- putStrLn . show $ qy'
+    -- putStrLn . show $ ry'
 
 app7 :: IO ()
 app7 = do
-    let x = fromList' 2 2 [1, 1, 0, 1] :: Mat Float
-        y = [1, 1] :: UArray Float
+    let x = fromList' 2 2 [1, 1, 0, 1] :: Mat Float 2 2
+        y = [1, 1] :: Vec Float 2
     putStrLn . show $ transform x y
 
-app8 :: IO ()
-app8 = do
-    let x = fromList' 3 2 [ 1.54818221,  0.42103339, -0.18791299,  0.72404307, -0.99845554, -0.2456467 ] :: Mat Float
-        (u, s, vt) = svd x
-        (u', s', vt') = svd' x
-    putStrLn . show $ u
-    putStrLn . show $ s
-    putStrLn . show $ vt
-    putStrLn . show $ u'
-    putStrLn . show $ s'
-    putStrLn . show $ vt'
-    putStrLn . show $ singular x
+-- app8 :: IO ()
+-- app8 = do
+--     let x = fromList' 3 2 [ 1.54818221,  0.42103339, -0.18791299,  0.72404307, -0.99845554, -0.2456467 ] :: Mat Float 3 2
+--         (u, s, vt) = svd x
+--         (u', s', vt') = svd' x
+--     putStrLn . show $ u
+--     putStrLn . show $ s
+--     putStrLn . show $ vt
+--     putStrLn . show $ u'
+--     putStrLn . show $ s'
+--     putStrLn . show $ vt'
+--     putStrLn . show $ singular x
 
 app9 :: IO ()
 app9 = do
     -- let x = fromList' 2 2 [ 1.54818221,  0.42103339, -0.18791299,  0.72404307 ] :: Mat Float
-    let x = fromList' 2 2 [ 4, 3, 7, 9 ] :: Mat Float
-        y = fromList' 2 2 [ 4, 3, 7, 9 ] :: Mat (Complex Float)
+    let x = fromList' 2 2 [ 4, 3, 7, 9 ] :: Mat Float 2 2
+        y = fromList' 2 2 [ 4, 3, 7, 9 ] :: Mat (Complex Float) 2 2
         (lam, ut, v) = eigen x
     putStrLn . show $ lam
     -- putStrLn . show $ ut
